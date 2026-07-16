6478bc89c82075d98606cbf6db92fe09567499c6
lip-vieira <filipeluis1110@gmail.com>
2026-07-16 00:22:55 -0300

    atualização do Quick Sort

--- ordenacao_avancada.cpp
+++ ordenacao_avancada.cpp
@@ -1,35 +1,29 @@
 #include "biblioteca.h"
 #include <vector>
 #include <utility>
+#include <ctime> // Necessário para usar clock() e clock_t da biblioteca <time.h>
 
-long comparacoes_globais = 0;
-long trocas_globais = 0;
-
-long* p_comparacoes = &comparacoes_globais;
-long* p_trocas = &trocas_globais;
-
+// Função auxiliar para o Quick Sort encontrar o pivô
 int particionar(std::vector<int>& v, int inicio, int fim, long &comparacoes, long &trocas) {
     int pivo = v[fim];
     int i = inicio - 1;
 
     for (int j = inicio; j < fim; j++) {
-        (*p_comparacoes)++;
-        comparacoes++;
+        comparacoes++; // Incrementa a comparação feita no 'if'
         if (v[j] <= pivo) {
             i++;
             std::swap(v[i], v[j]);
-            (*p_trocas)++;
-            trocas++;
+            trocas++; // Incrementa a troca de posição
         }
     }
     
     std::swap(v[i + 1], v[fim]);
-    (*p_trocas)++;
-    trocas++;
+    trocas++; // Incrementa a troca final para colocar o pivô no lugar correto
     
     return i + 1;
 }
 
+// Algoritmo Categoria 2: Quick Sort (Recursivo)
 void quickSort(std::vector<int>& v, int inicio, int fim, long &comparacoes, long &trocas) {
     if (inicio < fim) {
         int pi = particionar(v, inicio, fim, comparacoes, trocas);
@@ -37,11 +31,5 @@ void quickSort(std::vector<int>& v, int inicio, int fim, long &comparacoes, long
         quickSort(v, pi + 1, fim, comparacoes, trocas);
     }
 }
-
-
-
-
-
-
 
 // Desenvolve o menu e chama as funções aqui...
